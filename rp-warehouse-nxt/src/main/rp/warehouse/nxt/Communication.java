cc276c1d94019b1c137b60e8e4b3cc12e5aca727
david <davmatjo@gmail.com>
2018-02-26 20:36:31 +0000

    First implementation of communication

--- rp-warehouse-nxt/src/main/rp/warehouse/nxt/Communication.java
+++ rp-warehouse-nxt/src/main/rp/warehouse/nxt/Communication.java
@@ -1,4 +1,48 @@
 package rp.warehouse.nxt;
 
-public class Communication {
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Communication implements Runnable {
+    private final DataInputStream fromPC;
+    private final DataOutputStream toPC;
+
+    public Communication() {
+        System.out.println("Waiting on bluetooth");
+        BTConnection connection = Bluetooth.waitForConnection();
+        System.out.println("Connected");
+
+        fromPC = connection.openDataInputStream();
+        toPC = connection.openDataOutputStream();
+
+        Thread receive = new Thread(this);
+        receive.setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                int command = fromPC.readInt();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendCommand(int command) {
+        try {
+            toPC.write(command);
+            toPC.flush();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
 }
