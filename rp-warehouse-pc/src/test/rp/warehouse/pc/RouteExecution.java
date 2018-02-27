ee7a02bc8a0a12899d4d9ff675fd283f8a57efb9
roman <rxp777@student.bham.ac.uk>
2018-02-27 17:05:01 +0000

    Nothing yet

---
+++ rp-warehouse-pc/src/test/rp/warehouse/pc/RouteExecution.java
@@ -0,0 +1,17 @@
+package rp.warehouse.pc;
+
+public class RouteExecution implements RouteExecutionInterface {
+
+    public static void main(String[] args) {
+        //Robot robot = new Robot(id);
+        
+        //Assignment jobs = new Assignment(robot);
+        //Planning plan = new Planning(robot, jobs);
+        
+        
+        
+    }
+    
+    
+
+}
