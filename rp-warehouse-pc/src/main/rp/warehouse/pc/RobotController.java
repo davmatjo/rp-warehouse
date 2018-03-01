46745658594ee8e5f3afce76e946117ba7a10d6a
roman <rxp777@student.bham.ac.uk>
2018-03-01 13:40:33 +0000

    more stuff

---
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/RobotController.java
@@ -0,0 +1,42 @@
+package rp.warehouse.pc;
+/**
+ * 
+ * @author roman
+ *
+ */
+public class RobotController extends Thread {
+    
+    Robot nxtBrick = null;
+    public RobotController(Robot _nxtBrick) {
+        nxtBrick=_nxtBrick;
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        //move();
+        
+        //answer = getResponse();  // blocking
+        
+        //if (!answer){plan.replan}
+        //if(done){plan.more(Robot)}
+        
+        //
+        
+        
+    }
+    /**
+     * When called cancels Job of the current item
+     */
+    public void cancelJob() {
+        
+    }
+    /**
+     * 
+     */
+    public void itemComplete() {
+        
+    }
+
+}
--- rp-warehouse-pc/src/main/rp/warehouse/pc/RouteExecution.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/RouteExecution.java
@@ -1,20 +1,57 @@
 package rp.warehouse.pc;
+
 public class RouteExecution implements RouteExecutionInterface {
-    private static int numberOfRobots =1;
+    private static int numberOfRobots = 1;
 
     public static void main(String[] args) {
-        //Array of robot IDs
-        //Robot[] robots = new Robot[numberOfRobots];
-        String id ="1";
-        Robot robot = new Robot(id);
-        
-        //Assignment jobs = new Assignment(robot);
-        //Planning plan = new Planning(robot, jobs);
+        // JobInput job = new JobInput("filename");
+        // Reads everything into some kind of class
+        // should it return that class to then pass it to selection
         
         
+        // JobSellection select = new JobSelefction();
+        // accesses that class
         
+        
+        // Array of robot IDs
+        Robot[] robots = new Robot[numberOfRobots];
+        
+        for (int i = 0; i < robots.length; i++) {// pass this
+            robots[i]=new Robot(i+"");
+        }
+        
+
+        // Assignment jobs = new Assignment(robots); // type Robot[]
+        // should i pass map class as well
+        
+        // Planning plan = new Planning(robot, jobs);
+        
+        //plan.plan;
+
+        for (Robot nxt : robots) {
+            // iterates through Robots and makes them move
+            RobotController robot1 = new RobotController(nxt);//pass Planning
+            robot1.start();
+            nxt.move(0);
+        }
+        
+
+        
+        
+        
+        /*
+        try {
+            robot1.join();// waits for the thread to end
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        */
+        // pick up (number)
+        
+        // cancel
+        
+
     }
-    
-    
 
 }
