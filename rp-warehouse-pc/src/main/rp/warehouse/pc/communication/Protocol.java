1ae41706a491665b01d1a11a26ab70c0eb81ad4e
dxj786 <dxj786@student.bham.ac.uk>
2018-03-01 16:48:12 +0000

    whoops

--- rp-warehouse-pc/src/main/rp/warehouse/pc/communication/Protocol.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/communication/Protocol.java
@@ -1,5 +1,13 @@
 package rp.warehouse.pc.communication;
 
-public enum Protocol {
-    WAITING, OK, FAIL, NORTH, EAST, SOUTH, WEST, CANCEL
+public class Protocol {
+    public static final int WAITING = 0;
+    public static final int OK = 1;
+    public static final int FAIL = 2;
+    public static final int NORTH = 3;
+    public static final int EAST = 4;
+    public static final int SOUTH = 5;
+    public static final int WEST = 6;
+    public static final int CANCEL = 7;
+
 }
