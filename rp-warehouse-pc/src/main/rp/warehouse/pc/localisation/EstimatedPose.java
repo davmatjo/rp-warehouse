0f7f7d2cef818f03ee9e6d6f44d2f8f9765fb3b3
kom788 <kom788@student.bham.ac.uk>
2018-03-01 13:59:23 +0000

    Initial localisation commit

---
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/localisation/EstimatedPose.java
@@ -0,0 +1,63 @@
+package rp.warehouse.pc.localisation;
+
+import lejos.geom.Point;
+import lejos.robotics.navigation.Pose;
+
+public class EstimatedPose extends Pose {
+
+	// Currently unused class, may use later.
+
+	private float prob;
+
+	/**
+	 * A class to represent an estimated pose of the robot. Extends the default Pose
+	 * to include a probability.
+	 * 
+	 * @param x
+	 *            The X coordinate of the robot.
+	 * @param y
+	 *            The Y coordinate of the robot.
+	 * @param heading
+	 *            The direction of which the robot is facing.
+	 * @param probability
+	 *            The probability of this pose being correct.
+	 */
+	public EstimatedPose(float x, float y, float heading, float probability) {
+		super(x, y, heading);
+		this.prob = probability;
+	}
+
+	/**
+	 * Method to retrieve the probability of this pose being correct.
+	 * 
+	 * @return The probability of this pose being correct.
+	 */
+	public float getProbability() {
+		return this.prob;
+	}
+
+	/**
+	 * Method to alter the probability of this pose being correct.
+	 * 
+	 * @param probability
+	 *            The new probability to set.
+	 */
+	public void setProbability(float probability) {
+		this.prob = probability;
+	}
+
+	/**
+	 * Method to represent the pose as a point.
+	 * 
+	 * @return The point from the pose.
+	 */
+	public Point toPoint() {
+		return new Point(getX(), getY());
+	}
+
+	@Override
+	public String toString() {
+		return getX() + ";" + getY() + ";" + getHeading() + ";" + prob;
+	}
+
+}
---
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/localisation/Ranges.java
@@ -0,0 +1,69 @@
+package rp.warehouse.pc.localisation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ranges {
+
+	public final static int FRONT = 0, RIGHT = 1, BACK = 2, LEFT = 3;
+	private float[] ranges = new float[4];
+
+	public Ranges(float front, float right, float back, float left) {
+		this.ranges[0] = front;
+		this.ranges[1] = right;
+		this.ranges[2] = back;
+		this.ranges[3] = left;
+	}
+
+	/**
+	 * Method to get a stored range in a given direction.
+	 * 
+	 * @param direction
+	 *            The direction of which to return the range.
+	 * @return The range in the given direction.
+	 */
+	public float get(int direction) {
+		return ranges[direction];
+	}
+
+	/**
+	 * Method to get a list of available directions to travel in given the ranges.
+	 * 
+	 * @return The directions of which can be travelled without collisions.
+	 */
+	public List<Integer> getAvailableDirections() {
+		List<Integer> dirs = new ArrayList<Integer>();
+		if (ranges[0] > 0)
+			dirs.add(FRONT);
+		if (ranges[1] > 0)
+			dirs.add(RIGHT);
+		if (ranges[2] > 0)
+			dirs.add(BACK);
+		if (ranges[3] > 0)
+			dirs.add(LEFT);
+		return dirs;
+	}
+
+	/**
+	 * Method to rotate the ranges by a given angle.
+	 * 
+	 * @param ranges
+	 *            The ranges to rotate.
+	 * @param rot
+	 *            The angle of rotation.<br>
+	 *            0 = 0<br>
+	 *            1 = 90<br>
+	 *            2 = 180<br>
+	 *            3 = 270
+	 * @return The rotated version of the ranges.
+	 */
+	public static Ranges rotate(Ranges ranges, int rot) {
+		float[] store = new float[4];
+		store[(FRONT + rot) % 4] = ranges.get(FRONT);
+		store[(RIGHT + rot) % 4] = ranges.get(RIGHT);
+		store[(BACK + rot) % 4] = ranges.get(BACK);
+		store[(LEFT + rot) % 4] = ranges.get(LEFT);
+		return new Ranges(store[0], store[1], store[2], store[3]);
+	}
+
+}
