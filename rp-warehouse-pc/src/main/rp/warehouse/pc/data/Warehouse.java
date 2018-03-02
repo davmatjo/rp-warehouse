e8d8225d93ebc5bcd09934e928476d856c07e6db
kom788 <kom788@student.bham.ac.uk>
2018-03-02 11:47:00 +0000

    Made Warehouse more efficient? Ish?

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/Warehouse.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/Warehouse.java
@@ -1,7 +1,5 @@
 package rp.warehouse.pc.data;
 
-import java.util.ArrayList;
-
 import lejos.geom.Line;
 import lejos.geom.Rectangle;
 import rp.robotics.mapping.GridMap;
@@ -17,39 +15,16 @@ public class Warehouse {
 		final int gridWitdth = 12, gridHeight = 8;
 		final float cellSize = 0.30f;
 
-		// First ins 36 39 56 188
-
-		ArrayList<Line> lines = new ArrayList<Line>();
-
-		// these are the walls for the world outline
-		lines.add(new Line(0f, 0f, width, 0f));
-		lines.add(new Line(width, 0f, width, height));
-		lines.add(new Line(width, height, 0f, height));
-		lines.add(new Line(0f, height, 0f, 0f));
-
-		lines.add(new Line(0.31f, 0.29f, 0.31f, height - 0.61f));
-		lines.add(new Line(0.61f, 0.29f, 0.61f, height - 0.61f));
-		lines.add(new Line(0.31f, 0.29f, 0.61f, 0.29f));
-		lines.add(new Line(0.31f, height - 0.61f, 0.61f, height - 0.61f));
-
-		lines.add(new Line(1.24f, 0.28f, 1.24f, height - 0.62f));
-		lines.add(new Line(1.54f, 0.28f, 1.54f, height - 0.62f));
-		lines.add(new Line(1.24f, 0.28f, 1.54f, 0.28f));
-		lines.add(new Line(1.24f, height - 0.62f, 1.54f, height - 0.62f));
-
-		lines.add(new Line(2.14f, 0.29f, 2.14f, height - 0.61f));
-		lines.add(new Line(2.44f, 0.29f, 2.44f, height - 0.61f));
-		lines.add(new Line(2.14f, 0.29f, 2.44f, 0.29f));
-		lines.add(new Line(2.14f, height - 0.61f, 2.44f, height - 0.61f));
-
-		lines.add(new Line(3.08f, 0.30f, 3.08f, height - 0.60f));
-		lines.add(new Line(3.38f, 0.30f, 3.38f, height - 0.60f));
-		lines.add(new Line(3.08f, 0.30f, 3.38f, 0.30f));
-		lines.add(new Line(3.08f, height - 0.60f, 3.38f, height - 0.60f));
-
-		Line[] lineArray = new Line[lines.size()];
-
-		lines.toArray(lineArray);
+		final Line[] lineArray = new Line[] { new Line(0f, 0f, width, 0f), new Line(width, 0f, width, height),
+				new Line(width, height, 0f, height), new Line(0f, height, 0f, 0f),
+				new Line(0.31f, 0.29f, 0.31f, height - 0.61f), new Line(0.61f, 0.29f, 0.61f, height - 0.61f),
+				new Line(0.31f, 0.29f, 0.61f, 0.29f), new Line(0.31f, height - 0.61f, 0.61f, height - 0.61f),
+				new Line(1.24f, 0.28f, 1.24f, height - 0.62f), new Line(1.54f, 0.28f, 1.54f, height - 0.62f),
+				new Line(1.24f, 0.28f, 1.54f, 0.28f), new Line(1.24f, height - 0.62f, 1.54f, height - 0.62f),
+				new Line(2.14f, 0.29f, 2.14f, height - 0.61f), new Line(2.44f, 0.29f, 2.44f, height - 0.61f),
+				new Line(2.14f, 0.29f, 2.44f, 0.29f), new Line(2.14f, height - 0.61f, 2.44f, height - 0.61f),
+				new Line(3.08f, 0.30f, 3.08f, height - 0.60f), new Line(3.38f, 0.30f, 3.38f, height - 0.60f),
+				new Line(3.08f, 0.30f, 3.38f, 0.30f), new Line(3.08f, height - 0.60f, 3.38f, height - 0.60f) };
 
 		return new GridMap(gridWitdth, gridHeight, xInset, yInstet, cellSize,
 				new LineMap(lineArray, new Rectangle(0, 0, width, height)));
