0680a0b0066245a296b0092c40dedd7b1417156b
roman <rxp777@student.bham.ac.uk>
2018-03-02 09:31:48 +0000

    Missing a few classes

--- rp-warehouse-pc/src/main/rp/warehouse/pc/route/RobotController.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/route/RobotController.java
@@ -1,6 +1,7 @@
 package rp.warehouse.pc.route;
 
 import rp.warehouse.pc.data.Robot;
+import rp.warehouse.pc.data.Robot.Response;
 
 /**
  * 
@@ -8,39 +9,63 @@ import rp.warehouse.pc.data.Robot;
  *
  */
 public class RobotController extends Thread {
-    
+
     Robot robot = null;
+
     public RobotController(Robot _robot) {
-        robot=_robot;
+        robot = _robot;
         // TODO Auto-generated constructor stub
     }
-    
+
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        
-        //move();
-        
-        Object answer = robot.getResponse();  // blocking
-        
-        //if (!answer){plan.replan}
-        //if(done){plan.more(Robot)}
-        
-        //
-        
-        
+        Response answer = null;
+        while (true) {
+            // check if there are more items in queue
+            //
+
+            robot.getCurrentJob();// can't check if empty or not
+            // it doesn't return anything
+            
+            if (true) {// If nothing left in the currentRoute
+                switch (answer) {
+                case WAITING:
+
+                    break;
+                case FAIL:
+                    System.exit(1);
+                    break;
+                case OK:
+
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            
+            robot.move();
+            robot.getResponse(); // blocking
+        }
     }
+
     /**
      * When called cancels Job of the current item
      */
     public void cancelJob() {
-        
+        // currentItem.getJob()
+        // cancelltems.add(thatjob)
+
     }
+
     /**
-     * 
+     * marks CurrentItem as complete 
+     * and asks planning to re-plan 
      */
     public void itemComplete() {
-        
+        // currentItem = getItemfromList(next valid one);
+        // plan.planRoute(robot);
+
     }
 
 }
