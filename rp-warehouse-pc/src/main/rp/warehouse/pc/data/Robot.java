bdfa4bffffe3996c041ada50f1274adad666e5f7
roman <rxp777@student.bham.ac.uk>
2018-03-03 09:52:03 +0000

    Moved RobotController to robot

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/Robot.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/Robot.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 
 public class Robot implements Runnable{
 
+
     // Communications
     private final String ID; // Communication ID
     private final String name; // Communication name
