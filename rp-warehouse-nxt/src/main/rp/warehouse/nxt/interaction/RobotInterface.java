29f7d629b169b415c287b7a4ff5a799ce7e824ec
dxj786 <dxj786@student.bham.ac.uk>
2018-03-04 13:38:47 +0000

    update RobotInterface Interface

--- rp-warehouse-nxt/src/main/rp/warehouse/nxt/interaction/RobotInterface.java
+++ rp-warehouse-nxt/src/main/rp/warehouse/nxt/interaction/RobotInterface.java
@@ -4,4 +4,8 @@ public interface RobotInterface	{
 	
 	boolean buttonPressed();
 	void displayScreen();
+
+	// Wait for user to pick up the correct number of items, then return true if they do and
+    // false if they don't
+	boolean pickup(int countToPickup);
 }
