62b992cad101db4d1be370469d28fdccdc023ae9
roman <rxp777@student.bham.ac.uk>
2018-03-05 15:08:48 +0000

    Changed return type of getLocation()

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/Robot.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/Robot.java
@@ -99,7 +99,7 @@ public class Robot implements Runnable {
         return ID;
     }
 
-    public Location getLocation() {
+    public RobotLocation getLocation() {
         return location;
     }
 
