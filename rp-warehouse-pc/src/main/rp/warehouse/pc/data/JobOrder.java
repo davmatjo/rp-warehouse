b9c85aa370b9201cfa93a36f4e4d138fa58f31fc
Nikol Levunlieva <nikol.levunlieva@gmail.com>
2018-03-05 22:57:47 +0000

    added the jobs arraylist

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/JobOrder.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/JobOrder.java
@@ -1,17 +1,30 @@
 package rp.warehouse.pc.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class JobOrder implements Comparable<Job> {
 
-	private HashMap<String, Integer> picks = new HashMap<String, Integer>();
+	private HashMap<String, Integer> tasks = new HashMap<String, Integer>();
 	private float value;
+	private boolean cancelled;
+	private boolean predictedCancel;
+	private ArrayList<Job> jobs;
+
+	public JobOrder(HashMap<String, Integer> tasks, float value, boolean cancelled, boolean predictedCancel,
+			ArrayList<Job> jobs) {
+		this.tasks = tasks;
+		this.value = value;
+		this.cancelled = cancelled;
+		this.predictedCancel = predictedCancel;
+		this.jobs = jobs;
+	}
 
 	public void addPick(String item, int count) {
-		if (picks.containsKey(item)) {
-			picks.replace(item, count);
+		if (tasks.containsKey(item)) {
+			tasks.replace(item, count);
 		} else {
-			picks.put(item, count);
+			tasks.put(item, count);
 		}
 	}
 
@@ -27,7 +40,7 @@ public class JobOrder implements Comparable<Job> {
 
 	public int totalItems() {
 		int total = 0;
-		for (Integer i : picks.values()) {
+		for (Integer i : tasks.values()) {
 			total += i;
 		}
 		return total;
