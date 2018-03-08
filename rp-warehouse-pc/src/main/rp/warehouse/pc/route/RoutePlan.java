1ca5165e8be9ea390b220ad93783e4732a134d04
Ali Hejazi <axh965@student.bham.ac.uk>
2018-03-08 20:06:34 +0000

    Added javadocs comments to RoutePlan and RoutePlanLocation

--- rp-warehouse-pc/src/main/rp/warehouse/pc/route/RoutePlan.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/route/RoutePlan.java
@@ -10,9 +10,17 @@ import rp.warehouse.pc.data.Robot;
 import rp.warehouse.pc.data.RobotLocation;
 import rp.warehouse.pc.data.Warehouse;
 
+/**
+ * Used to plan the route to the goal location.
+ * 
+ * The route plan commands are sent to route execution as a queue of integers, where different integers represent north, east, south, and west.
+ * 
+ * @author alihejazi
+ *
+ */
+
 public class RoutePlan {
 	private Robot robot;
-	//private Map map;
 	private Location goalLocation;
 	//private ArrayList<Location> blocked = Warehouse.getBlockedLocations();
 	
@@ -25,34 +33,56 @@ public class RoutePlan {
 	boolean goalNotFound = true;
 	static Queue<Integer> plan = new LinkedList<Integer>();
 	
-	public RoutePlan(Robot robot,/*Map map,*/ Location goalLocation) {
+	/**
+	 * The overloaded constructor for the RoutePlan class
+	 * @param robot the robot is passed so that we can access its current coordinates to plan its route
+	 * @param goalLocation the goal location is passed so that we can plan a route to it
+	 */
+	public RoutePlan(Robot robot, Location goalLocation) {
 		this.robot = robot;
-		//this.map = map;
 		this.goalLocation = goalLocation;
 		
 		Location currentRobotLocation = this.robot.getLocation();
 		currentX = currentRobotLocation.getX();
 		currentY = currentRobotLocation.getY();
-		//currentDirection = currentRobotLocation.getDirection();
 		
 		run();
 	}
 	
+	/**
+	 * A static method returning a queue of integers, representing commands for route execution to understand
+	 * @param robot the robot is passed so that we can pass it the overloaded constructor to access its current coordinates to plan its route
+	 * @param goalLocation the goal location is passed so that we can plan a route to it
+	 * @return we return 'plan' - a queue of integer route instructions for RouteExecution to understand
+	 */
 	public static Queue<Integer> plan(Robot robot, Location goalLocation) {
 		RoutePlan routePlan = new RoutePlan(robot, goalLocation);
 		Queue<Integer> plan = RoutePlan.plan;
-		RoutePlan.plan = new LinkedList<>();
+		RoutePlan.plan = new LinkedList<Integer>();
 		return plan;
 	}
 	
+	/**
+	 * A method to return a queue of integers (commands) on how to reach the next drop off point.
+	 * @param robot The robot is passed so that we know its current location, in order to plan the route for the next drop off point
+	 * @return we return 'plan' - a queue of integer route instructions for RouteExecution to understand
+	 */
 	public static Queue<Integer> planDropOff(Robot robot) {
 		return null;
 	}
 	
+	/**
+	 * A method to return the array list of visited locations
+	 * @return we return the array list of visited locations
+	 */
 	public ArrayList<Location> getVisitedList() {
 		return visitedList;
 	}
 	
+	/**
+	 * This method basically calls all the methods methods to plan all the next coordinated to visit to arrive at the goal location
+	 * These locations are converted into commands that RouteExecution understands
+	 */
 	public void run() {
 		
 		while (goalNotFound) {
@@ -73,13 +103,18 @@ public class RoutePlan {
 		}
 		
 		//now, we're out of the while loop - the goal has been found.
-		//the locations to get to the goal are now stored in order in the 'visitedList' array list
+		//the locations to get to the goal have now been stored in order in the 'visitedList' array list
 		
-		//now, we need to convert the list of Locations to the goal to commands that RouteExecution can understand.
-		
-		//send the commandsList to RouteExecution.
 	}
 	
+	
+	/**
+	 * A method to add each of the north, east, south, and west locations to the 'toVisitList' array list, if it is valid.
+	 * This method also checks if the location passed is the goal location, in which case we break the loop in the run method.
+	 * However, this is done after checking the validity of the location, so that it is added to the 'toVisitList' array list
+	 * before we break the loop.
+	 * @param location
+	 */
 	public void addToList(RoutePlanLocation location) {
 	
 		//check the location given to see if it is valid before proceeding.
@@ -98,17 +133,24 @@ public class RoutePlan {
 		}
 	}
 	
+	/**
+	 * A method to check if the location checked is valid or not. If valid, we may consider it for the next move, as we put it in the
+	 * 'toVisitList' array list.
+	 * @param location The location being tested on its validity.
+	 * @return We return true if the location is valid, and false otherwise.
+	 */
 	public boolean isValid(RoutePlanLocation location) {
 		
 		int x = location.getX();
 		int y = location.getY();
 		
-		//assuming the grid size
-		final int MAX_X = 10;
-		final int MAX_Y = 10;
+		final int MAX_X = 11;
+		final int MAX_Y = 7;
 		
 		boolean locationIsBlocked = false;
 
+		
+		//PLEASE DO NOT DELETE THIS
 		/*for (int i = 0; i < blocked.size(); i ++) {
 			if ((blocked.get(i).getX() == x && blocked.get(i).getY() == y)) {
 				locationIsBlocked = true;
@@ -124,23 +166,25 @@ public class RoutePlan {
 		}*/
 		
 		
-		if (
-			x >= 0
+		if (x >= 0
 			&& y >= 0
 			&& x <= MAX_X
 			&& y <= MAX_Y
 			&& visitedList.contains(location) == false
-			&& locationIsBlocked == false
-			)
+			&& locationIsBlocked == false)
 			{	
-				//now check for obstacles (can't do now because I don't have the map)
-				//might even need Bluetooth to check for robots in the path
+				//might need Bluetooth to check for robots in the path when doing multi-route planning
 				return true;
 			}
 		
 		else return false;	
 	}
 	
+	/**
+	 * A method to check if the location passed is the goal location.
+	 * @param location The location being checked.
+	 * @return We return true if this location is the goal location, and false otherwise.
+	 */
 	public boolean isGoal(Location location) {
 		int x = location.getX();
 		int y = location.getY();
@@ -154,6 +198,12 @@ public class RoutePlan {
 		else return false;
 	}
 	
+	
+	/**
+	 * A method to move to the cheapest-cost valid location available.
+	 * @param prevX This is the previous location's x coordinate
+	 * @param prevY This is the previous location's y coordinate
+	 */
 	public void visitCheapestLocationBasedOnPathCost(int prevX, int prevY) {
 		
 		RoutePlanLocation cheapestLocation = toVisitList.get(0);
@@ -174,14 +224,15 @@ public class RoutePlan {
 		
 		System.out.println("Visited: " + cheapestLocation);
 		visitedList.add(cheapestLocation);
-		cheapestLocation.setHasBeenVisited(true);
+		//cheapestLocation.setHasBeenVisited(true);
 		//blocked.add(cheapestLocation);
 		
 		//update the location so now we're at the new location we're moving to
 		currentX = cheapestLocation.getX();
 		currentY = cheapestLocation.getY();
-		//currentDirection = cheapestLocation.getDirection();
 		
+		
+		//now, we convert the Location to a command that RouteExecution can understand
 		
 		if (currentX - prevX == 1) {
 			plan.offer(Protocol.EAST);
@@ -193,9 +244,7 @@ public class RoutePlan {
 			plan.offer(Protocol.SOUTH);
 		}
 		
-		
-		
-		
+		//reset the array list
 		toVisitList = new ArrayList<RoutePlanLocation>();
 	}
 	
