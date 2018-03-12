984663a58a98f396e4914a5ca518db4a395416b7
dxj786 <dxj786@student.bham.ac.uk>
2018-03-12 10:33:20 +0000

    undo cancellations changes

---
+++ rp-warehouse-pc/src/test/rp/warehouse/pc/communication/CommunicationTestSingle.java
@@ -0,0 +1,28 @@
+package rp.warehouse.pc.communication;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommunicationTestSingle {
+
+    @Before
+
+
+    @Test
+    public void sendMovement() {
+    }
+
+    @Test
+    public void sendPickupRequest() {
+    }
+
+    @Test
+    public void getRanges() {
+    }
+
+    @Test
+    public void close() {
+    }
+}
