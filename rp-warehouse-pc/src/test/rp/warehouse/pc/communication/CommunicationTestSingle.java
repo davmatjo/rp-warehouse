7281e47e14d2f374dbd9888f8e90bfe7c7463921
dxj786 <dxj786@student.bham.ac.uk>
2018-03-12 11:08:33 +0000

    JUnit communication single robot

--- rp-warehouse-pc/src/test/rp/warehouse/pc/communication/CommunicationTestSingle.java
+++ rp-warehouse-pc/src/test/rp/warehouse/pc/communication/CommunicationTestSingle.java
@@ -1,28 +1,51 @@
 package rp.warehouse.pc.communication;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import rp.warehouse.pc.data.Robot;
+import rp.warehouse.pc.localisation.Ranges;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.mockito.Mockito.mock;
 
 public class CommunicationTestSingle {
+    private static Communication communication;
+    private static final String[] robotNames = new String[] {"ExpressBoi", "Meme Machine", "Orphan"};
+    private static final String[] robotIDs = new String[] {"0016531AFBE1", "0016531501CA", "0016531303E0"};
 
-    @Before
-
+    @BeforeClass
+    public static void setup() throws Exception {
+        communication = new Communication(robotIDs[0], robotNames[0], mock(Robot.class));
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(communication);
+    }
 
     @Test
     public void sendMovement() {
+        communication.sendMovement(Protocol.NORTH);
+        communication.sendMovement(Protocol.EAST);
+        communication.sendMovement(Protocol.SOUTH);
+        communication.sendMovement(Protocol.WEST);
     }
 
     @Test
     public void sendPickupRequest() {
+        int response = communication.sendPickupRequest();
+        Assert.assertTrue(response > 0);
     }
 
     @Test
     public void getRanges() {
+        Ranges response = communication.getRanges();
+        Assert.assertNotNull(response);
     }
 
     @Test
     public void close() {
+        communication.close();
     }
 }
