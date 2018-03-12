69ca4e6180f1cd373c4554c81d0d99c932d7f417
Nikol Levunlieva <nikol.levunlieva@gmail.com>
2018-03-12 10:25:55 +0000

    added logs

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/JobSelector.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/JobSelector.java
@@ -1,6 +1,7 @@
 package rp.warehouse.pc.data;
 
 import java.util.ArrayList;
+import org.apache.log4j.Logger;
 import java.util.Collections;
 
 import rp.warehouse.pc.route.*;
@@ -11,6 +12,7 @@ public class JobSelector {
 	private boolean cancelled;
 	private boolean predictedCancel;
 	private float value;
+	private final static Logger logger = Logger.getLogger(JobSelector.class);
 	
 	public JobSelector(ArrayList<Job> jobs, int cancelled, boolean predictedCancel, float value) {
 		this.jobs = jobs;
@@ -35,28 +37,28 @@ public class JobSelector {
 		return this.cancelled;
 	}
 
-	//A method for calculating the overall reward for a given Job j. 
 	public float totalReward(Job j) {
 		float total = 0;
 		for (Task t : j.getItems()) {
-			total = total + t.getItem().getReward() * t.getCount(); //Gets each item's reward and multiplies it by the item's count.
-																	//All the total methods follow a similar logic.
+			logger.trace("Calculating total reward of given job.");
+			total = total + t.getItem().getReward() * t.getCount(); 
+			logger.trace("Logic: item reward multiplied by item count.");
 		}
 		return total = 0;
 	}
 	
 	
-	//A method for counting how many items overall a job j needs to get.
 	public int totalItems(Job j) {
 		int total = 0;
 		for (Task t : j.getItems()) {
+			logger.trace("Calculating count of items given job has.");
 			total = total + t.getCount();
 		}
 		return total;
 	}
 	
-	// The method that sorts the ArrayList of Jobs based on their total reward (for now). Will be extended to reward per coordinate.
 	public void sortByReward() {
+		logger.trace("Sorting jobs based on total reward.");
 		Collections.sort(jobs, (a, b) -> (int)totalReward(b) - (int)totalReward(a));
 	}
 }
