5c93471751e459ef99a7587aaba7b2826cf1a7a4
dxj786 <dxj786@student.bham.ac.uk>
2018-03-13 19:56:06 +0000

    JUnit tests for Location improvements

--- rp-warehouse-pc/src/test/rp/warehouse/pc/localisation/TestLocaliser.java
+++ rp-warehouse-pc/src/test/rp/warehouse/pc/localisation/TestLocaliser.java
@@ -7,7 +7,7 @@ public class TestLocaliser {
 
 	public static void main(String[] args) {
 		Localisation local = new Localiser(null);
-		System.out.println(local.getPosition());
+		//System.out.println(local.getPosition());
 	}
 
 }
