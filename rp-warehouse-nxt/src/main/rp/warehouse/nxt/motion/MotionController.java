aab710546afe10062f1e4a4d12a13819d96c4923
dxj786 <dxj786@student.bham.ac.uk>
2018-03-15 12:33:53 +0000

    robot motion

--- rp-warehouse-nxt/src/main/rp/warehouse/nxt/motion/MotionController.java
+++ rp-warehouse-nxt/src/main/rp/warehouse/nxt/motion/MotionController.java
@@ -110,7 +110,7 @@ public class MotionController implements Movement {
 		boolean junction = false;
 
 		pilot.rotate(rotation);
-		pilot.setTravelSpeed(0.1);
+		pilot.setTravelSpeed(0.15);
 		pilot.forward();
 
 		Rate r = new Rate(20);
@@ -127,9 +127,9 @@ public class MotionController implements Movement {
 			}
 			// check is robot has gone off the line and adjust
 			else if (leftValue < leftLineLimit) {
-				pilot.steer(50);
+				pilot.steer(35);
 			} else if (rightValue < rightLineLimit) {
-				pilot.steer(-50);
+				pilot.steer(-35);
 				;
 			} else {
 				pilot.steer(0);
