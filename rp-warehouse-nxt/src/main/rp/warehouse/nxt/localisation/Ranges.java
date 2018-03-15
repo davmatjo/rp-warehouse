cce8783e483617b3acd1e21763e6380c88586b68
Kieran <KOM788@student.bham.ac.uk>
2018-03-15 04:41:09 +0000

    Change mean to mode in NXT ranges

--- rp-warehouse-nxt/src/main/rp/warehouse/nxt/localisation/Ranges.java
+++ rp-warehouse-nxt/src/main/rp/warehouse/nxt/localisation/Ranges.java
@@ -6,25 +6,42 @@ import lejos.util.Delay;
 import rp.warehouse.nxt.motion.MotionController;
 
 public class Ranges {
-    private final MotionController motion;
-    private final UltrasonicSensor sensor;
+	private final MotionController motion;
+	private final UltrasonicSensor sensor;
 
-    public Ranges(MotionController motion) {
-        this.motion = motion;
-        this.sensor = new UltrasonicSensor(SensorPort.S3);
-    }
+	public Ranges(MotionController motion) {
+		this.motion = motion;
+		this.sensor = new UltrasonicSensor(SensorPort.S3);
+	}
 
-    public float[] getRanges() {
-        float[] ranges = new float[4];
-        for (int i=0; i<4 ; i++) {
-            float totalRange = 0f;
-            for (int j=0; j < 10; j++) {
-                Delay.msDelay(20);
-                totalRange += sensor.getRange();
-            }
-            ranges[i] = totalRange / 10f;
-            motion.rotate();
-        }
-        return ranges;
-    }
+	public float[] getRanges() {
+		float[] ranges = new float[4];
+		for (int i = 0; i < 4; i++) {
+			float[] tempRanges = new float[10];
+			for (int j = 0; j < 10; j++) {
+				Delay.msDelay(20);
+				tempRanges[j] = sensor.getRange();
+			}
+			ranges[i] = mode(tempRanges);
+			motion.rotate();
+		}
+		return ranges;
+	}
+
+	private static float mode(float a[]) {
+		float maxValue = 0;
+		int maxCount = 0;
+		for (int i = 0; i < a.length; ++i) {
+			int count = 0;
+			for (int j = 0; j < a.length; ++j) {
+				if (a[j] == a[i])
+					++count;
+			}
+			if (count > maxCount) {
+				maxCount = count;
+				maxValue = a[i];
+			}
+		}
+		return maxValue;
+	}
 }
