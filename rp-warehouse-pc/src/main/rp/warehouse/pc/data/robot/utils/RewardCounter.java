18eda11fd73c85368fd5f9c00aa5bce8dbbe1245
Roman <romanpodkovyrinr@gmail.com>
2018-03-20 07:06:38 +0000

    Reward Counter working

--- rp-warehouse-pc/src/main/rp/warehouse/pc/data/robot/utils/RewardCounter.java
+++ rp-warehouse-pc/src/main/rp/warehouse/pc/data/robot/utils/RewardCounter.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import rp.warehouse.pc.data.Task;
+import rp.warehouse.pc.data.robot.Robot;
 import rp.warehouse.pc.input.Job;
 import rp.warehouse.pc.input.Jobs;
 import rp.warehouse.pc.management.providers.main.WarehouseInfoListener;
@@ -22,6 +25,8 @@ public class RewardCounter {
     private static final Map<String, Boolean> cancelledJobReference = new HashMap<>();
     private static final Map<String, Boolean> completedJobReference = new HashMap<>();
     private static final List<WarehouseInfoListener> listeners = new ArrayList<>();
+    
+    private static final Logger logger = Logger.getLogger(RewardCounter.class);
 
     private static float pointsEarned = 0.0f;
     
@@ -36,7 +41,7 @@ public class RewardCounter {
         return completedJobReference.size();
     }
     
-    private static int getnumberJobsCancelled() {
+    private static int getNumberJobsCancelled() {
         return cancelledJobReference.size();
     }
     
@@ -44,10 +49,14 @@ public class RewardCounter {
         return pointsEarned;
     }
     
+    public static float getNumberUncompletedJobs() {
+        return uncompletedJobReference.size();
+    }
+    
     public synchronized static void addCancelledJob(Task task) {
         cancelledJobReference.put(task.getJobID(), true);
         for (WarehouseInfoListener listener : listeners) {
-            listener.cancelledJobsChanged(getnumberJobsCancelled());
+            listener.cancelledJobsChanged(getNumberJobsCancelled());
         }
     }
     
@@ -56,7 +65,7 @@ public class RewardCounter {
         return cancelledJobReference.containsKey(task.getJobID());
     }
 
-    public synchronized static void addReward(float reward) {
+    private synchronized static void addReward(float reward) {
         pointsEarned += reward;
         for (WarehouseInfoListener listener : listeners) {
             listener.rewardChanged(getPointsEarned());
@@ -67,27 +76,38 @@ public class RewardCounter {
         String jobId= task.getJobID();
         if (!checkIfCancelled(task) && jobReference.containsKey(jobId)) {
             if(uncompletedJobReference.containsKey(jobId)) {
+                
+                int numberOfTaskDone = uncompletedJobReference.get(jobId);
                 if(jobReference.get(jobId).numOfTasks() > uncompletedJobReference.get(jobId)) {
-                    uncompletedJobReference.put(jobId, uncompletedJobReference.get(jobId)+1);
+                    uncompletedJobReference.put(jobId, numberOfTaskDone+1);
                 } 
-                if (jobReference.get(jobId).numOfTasks() == uncompletedJobReference.get(jobId)) {
-                    uncompletedJobReference.remove(jobId);
-                    completedJobReference.put(jobId, true);
-                    Job rewardToClaim = jobReference.get(jobId);
-                    ArrayList<Task> tasks = rewardToClaim.getItems();
-                    for (Task taskElement: tasks) {
-                        addReward(taskElement.getItem().getReward());
-                    }
-                }
+                
             }else {
                 uncompletedJobReference.put(jobId, 1);
             }
             
+            if (jobReference.get(jobId).numOfTasks() == uncompletedJobReference.get(jobId)) {
+                
+                uncompletedJobReference.remove(jobId);
+                
+                completedJobReference.put(jobId, true);
+                Job rewardToClaim = jobReference.get(jobId);
+                ArrayList<Task> tasks = rewardToClaim.getItems();
+                logger.debug("Job " + jobId + " completed");
+                for (Task taskElement: tasks) {
+                    float earnedRewards = taskElement.getItem().getReward() * taskElement.getCount();
+                    addReward(earnedRewards);
+                    logger.debug("Earned " + earnedRewards + " points for Item(s) " + taskElement.getItem());
+                }
+            }
+            
+        } else {
+            logger.debug("Completed task with Job ID: " + jobId + "  Is refused");
         }
 
         for (WarehouseInfoListener listener : listeners) {
             listener.jobCountChanged(getJobsDone());
-            listener.cancelledJobsChanged(getnumberJobsCancelled());
+            listener.cancelledJobsChanged(getNumberJobsCancelled());
         }
 
     }
